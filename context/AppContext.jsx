8a54ee9e40f42438246a3376ef10c7935aff4c85
adarshrai16 <adarshraiii457@gmail.com>
2026-08-27 05:58:38 +0530

    Add files via upload

---
+++ context/AppContext.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import { createContext } from 'react'
+import { dummyCourses } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+export const AppContext = createContext()
+
+export const AppContextProvider = (props)=>{
+
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const navigate = useNavigate()
+
+     const [allCourses, setAllCourses] =useState([])
+     const [isEducator, setIsEducator]= useState(true)
+
+    //fetch all courses
+    const fetchAllcourses = async ()=>{
+        setAllCourses(dummyCourses)
+    }
+
+    //Function to calculate average rating of course
+    const calculateRating =(course)=>{
+        if(!course.courseRatings || course.courseRatings.length === 0){
+            return 0;
+        }
+        let totalRating =0
+        course.courseRatings.forEach(rating=>{
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
+
+    useEffect(()=>{
+        fetchAllcourses()
+    },[])
+
+    const value={
+        currency, allCourses, navigate , calculateRating,
+        isEducator, setIsEducator
+    }
+
+    return(
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
